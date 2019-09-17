b869b9ff3035f37935c04b6a354ea41da47bed25
Hilary Zen <hzen00@stuy.edu>
2019-09-16 21:29:06 -0400

    Added comments

--- 03_csv/python4_niA-zenH.py
+++ 03_csv/python4_niA-zenH.py
@@ -6,19 +6,22 @@ def randomOccupations():
     occupations.pop(0)
     occupations.pop(len(occupations) - 1)
 
-    #Create dict
+    # Create dict
     occupationsDict = {}
     for job in occupations:
         job = job.strip()
         jobArray = job.rsplit(',', 1)
         occupationsDict[jobArray[0]] = jobArray[1]
 
+    # Pick a random float between 0 and 99.8
     randomJob = random.randint(1, 998) / 10.0
-    sum = 0
+    #print(randomJob)
 
+    # Add up the percentages of the jobs, and when sum is greater than randomJob return the occupation
+    sum = 0
     for job in occupationsDict:
         sum += float(occupationsDict[job])
-        if sum > randomJob:
+        if sum >= randomJob:
             return job
 
 print(randomOccupations())
