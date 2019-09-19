654770f29d2b5e7dc9d7f0ab888b28da7ef40681
Hilary Zen <hzen00@stuy.edu>
2019-09-19 02:13:17 -0400

    Added routes

--- 08_app0/app.py
+++ 08_app0/app.py
@@ -2,10 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello_world():
     print(__name__)
     return "no hablo queso!"
+
+@app.route("/french")
+def hello_france():
+    print(__name__)
+    return "je ne parle pas de fromage!"
+
+@app.route("/italian")
+def hello_italy():
+    print(__name__)
+    return "non parlo di formaggio!"
 
 if __name__ == "__main__":
     app.debug = True
