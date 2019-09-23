d039fc582f0a6cfad08a32463b4d3f5521989676
Hilary Zen <hzen00@stuy.edu>
2019-09-23 09:07:05 -0400

    Added occ

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def occupations():
     print(__name__)
-    return "no hablo queso!"
+    f = open("occupations.csv", "r")
+    occupations = f.readlines()
+    occupations.pop(0)
+    occupations.pop(len(occupations) - 1)
+
+    # Create dict
+    occupationsDict = {}
+    for job in occupations:
+        job = job.strip()
+        jobArray = job.rsplit(',', 1)
+        occupationsDict[jobArray[0]] = jobArray[1]
+
+    # Pick a random float between 0 and 99.8
+    randomJob = random.randint(1, 998) / 10.0
+    #print(randomJob)
+
+    # Add up the percentages of the jobs, and when sum is greater than randomJob return the occupation
+    sum = 0
+    for job in occupationsDict:
+        sum += float(occupationsDict[job])
+        if sum >= randomJob:
+            return job
 
 if __name__ == "__main__":
     app.debug = True
