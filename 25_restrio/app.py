f59eaf654212e7577eee9385ff8f66f32c03a968
Hilary Zen <hzen00@stuy.edu>
2019-11-14 00:33:25 -0500

    Finished three sites

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -14,6 +14,26 @@ def loripsum():
     #data = json.loads(response)
     return render_template('index.html', text = response)
 
+@app.route('/nhl')
+def nhl():
+    u = urlopen('https://statsapi.web.nhl.com/api/v1/teams?teamId=4,5,6')
+    response = u.read()
+    data = json.loads(response)
+    teamNames = []
+    teamList = data["teams"]
+    for t in teamList:
+        teamNames.append(t["name"])
+    return render_template('nhl.html', teams = teamNames)
+
+@app.route('/quote')
+def quote():
+    u = urlopen('http://quotes.rest/qod.json')
+    response = u.read()
+    data = json.loads(response)
+    quote = data["contents"]["quotes"][0]["quote"]
+    author = data["contents"]["quotes"][0]["author"]
+    return render_template('quote.html', quote = quote, author = author)
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
