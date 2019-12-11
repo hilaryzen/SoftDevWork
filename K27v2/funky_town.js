9434cafd62303da4421d4222552a09abf13ecb64
Hilary Zen <hzen00@stuy.edu>
2019-12-11 09:12:44 -0500

    Combined

---
+++ K27v2/funky_town.js
@@ -0,0 +1,26 @@
+var fibonacci = function(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+var gcd = function(a,b) {
+  var x;
+  var ans = 1;
+  for (x = 2; x <= Math.min(a,b); x++) {
+    if (a % x == 0 && b % x == 0) {
+      ans = x;
+    }
+  }
+  return ans;
+}
+
+var randomStudent = function() {
+  var names = ["Amy", "Bob", "Claire", "Dave"];
+  var index = Math.floor(Math.random() * names.length);
+  return names[index];
+}
