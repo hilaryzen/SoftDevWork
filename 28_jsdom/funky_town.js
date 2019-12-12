f8c7e078084e029ec6c82b6e56413a69356e0188
Hilary Zen <hzen00@stuy.edu>
2019-12-12 01:59:28 -0500

    Finished

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -8,6 +8,13 @@ var fibonacci = function(n) {
   }
 }
 
+var fibClick = function() {
+  var display = document.getElementById("display");
+  var ans = fibonacci(10);
+  console.log(ans);
+  display.innerHTML = ans;
+}
+
 var gcd = function(a,b) {
   var x;
   var ans = 1;
@@ -16,15 +23,32 @@ var gcd = function(a,b) {
       ans = x;
     }
   }
+  console.log(ans);
   return ans;
 }
 
-var randomButton = document.getElementById("random");
-randomButton.addEventListener("click", randomStudent);
+var gcdClick = function() {
+  var display = document.getElementById("display");
+  display.innerHTML = gcd(4,20);
+}
 
 var randomStudent = function() {
   var names = ["Amy", "Bob", "Claire", "Dave"];
   var index = Math.floor(Math.random() * names.length);
-  console.log("Console: " + names[index]);
+  console.log(names[index]);
   return names[index];
 }
+
+var randomClick = function() {
+  var display = document.getElementById("display");
+  display.innerHTML = randomStudent();
+}
+
+var fibButton = document.getElementById("fib");
+fibButton.addEventListener("click", fibClick);
+
+var gcdButton = document.getElementById("gcd");
+gcdButton.addEventListener("click", gcdClick);
+
+var randomButton = document.getElementById("random");
+randomButton.addEventListener("click", randomClick);
