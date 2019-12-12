f0adb52ea1d27baacc8768d2fcd146baed7f2e3a
Hilary Zen <hzen00@stuy.edu>
2019-12-12 09:31:49 -0500

    Started functions

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,4 +1,17 @@
 var changeHeading = function(e) {
   var h = document.getElementById("h");
-  h.innerHTML = e;
+  h.innerHTML = e.innerHTML;
 };
+
+var removeItem = function(e) {
+  e.remove();
+}
+
+var lis = document.getElementsByTagName("li");
+
+for (var i = 0; i < lis.length; i++) {
+  console.log(lis[i]);
+  lis[i].addEventListener('mouseover', changeHeading(lis[i]));
+  lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World";});
+  lis[i].addEventListener('click', removeItem(lis[i]));
+}
