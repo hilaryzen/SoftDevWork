c7d5d409b70ae98bc0bd00e54066108950de3a62
Hilary Zen <hzen00@stuy.edu>
2020-02-12 13:02:44 -0500

    Finished work 6

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,10 +1,34 @@
-var c = document.getElementById("slate");
+var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
-var isToggled = true;
+var lastX = -1;
+var lastY = -1;
 
 var clear = function() {
   ctx.clearRect(0, 0, c.width, c.height);
+  lastX = -1;
+  lastY = -1;
+}
+
+var dot = function(){
+  //Starts a new path
+  ctx.beginPath();
+  ctx.arc(event.offsetX, event.offsetY, 10, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
+
+var draw = function() {
+  dot();
+  if (lastX > 0) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  }
+  lastX = event.offsetX;
+  lastY = event.offsetY;
 }
 
 var clean = document.getElementById("clean");
 clean.addEventListener("click", clear);
+c.addEventListener("click", draw)
