ea35f2e381407462b8d4a631455205e07b2c33a6
Hilary Zen <hzen00@stuy.edu>
2020-02-27 09:21:16 -0500

    Reading from json

--- 09_mongo/restaurants.py
+++ 09_mongo/restaurants.py
@@ -1,3 +1,21 @@
+from pymongo import MongoClient
+import json
+
+server_address = '127.0.0.1'
+
+c = MongoClient(server_address, 27017)
+db = c["test"]
+col = db["restaurants"]
+
 f = open("data.json", "r")
 restaurants = f.readlines()
+count = 0
+for r in restaurants:
+    print(count)
+    dict = eval(r.strip())
+    print(dict)
+    col.insert_one(dict)
+    count += 1
+
+print(db.restaurants.count_documents({}))
 f.close()
