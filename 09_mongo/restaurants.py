2f5353d90aaceb3596b39adae2b0df0ef80596c4
Hilary Zen <hzen00@stuy.edu>
2020-02-27 21:54:01 -0500

    Parsing

--- 09_mongo/restaurants.py
+++ 09_mongo/restaurants.py
@@ -1,5 +1,11 @@
 from pymongo import MongoClient
-import json
+
+from bson.json_util import loads
+
+def ingest(f):
+    fmt = ''
+    with open(f) as _f:
+        return loads(f'[{",".join(map(lambda s: s[:len(s) - 1], _f))}]')
 
 server_address = '127.0.0.1'
 
@@ -7,15 +13,17 @@ c = MongoClient(server_address, 27017)
 db = c["test"]
 col = db["restaurants"]
 
-f = open("data.json", "r")
-restaurants = f.readlines()
-count = 0
-for r in restaurants:
-    print(count)
-    dict = eval(r.strip())
-    print(dict)
-    col.insert_one(dict)
-    count += 1
+# f = open("data.json", "r")
+# restaurants = f.readlines()
+# count = 0
+# for r in restaurants:
+#     print(count)
+#     dict = eval(r.strip())
+#     print(dict)
+#     col.insert_one(dict)
+#     count += 1
 
-print(db.restaurants.count_documents({}))
+restaurants = ingest("data.json")
+col.insert(restaurants)
+print(col.count_documents({}))
 f.close()
