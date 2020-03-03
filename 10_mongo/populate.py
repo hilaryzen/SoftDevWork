d5aec467906879c84f98b56f2a0e80bcbbab5cbf
Hilary Zen <hzen00@stuy.edu>
2020-03-03 17:52:50 -0500

    Added comments

--- 10_mongo/populate.py
+++ 10_mongo/populate.py
@@ -8,6 +8,10 @@ client = MongoClient()
 db = client.stickySulphur
 col = db.senators
 
+# Our dataset is Current US Senators: https://www.govtrack.us/api/v2/role?current=true&role_type=senator
+# It contains all 100 members of the current Senate, and basic information like their state, birth date, gender, party, office location, etc.
+# We used the json library to read in the file, and then inserted each senator's data one by one into the database
+
 with open("./senators.json", 'r') as file:
     data = json.load(file)
     #print(data['objects'][0])
