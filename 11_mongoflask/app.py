d43c4743656f3a26da8b3e910b2b4437d9a14398
Hilary Zen <hzen00@stuy.edu>
2020-03-10 09:09:45 -0400

    Added form

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+import methods
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/stickySulphur"
 mongo = PyMongo(app)
 
 @app.route("/")
 def hello_world():
+    return render_template("home.html")
+
+@app.route("/name")
+def name():
     fname = "Lamar"
     lname = "Alexander"
     # find_one returns a dict instead of a cursor object
     senator = mongo.db.senators.find_one({"person.firstname": fname, "person.lastname": lname})
-    return render_template("home.html", result = senator["description"])
+    return render_template("results.html")
 
 if __name__ == "__main__":
     app.debug = True
---
+++ 11_mongoflask/methods.py
@@ -0,0 +1,48 @@
+#Tyler Huang, Jacob Olin, Hillary Zed
+#Team Sticky Sulphur
+#SoftDev pd 2
+#K10 -- Import/Export Bank
+#2020-03-04
+
+from pymongo import MongoClient
+from pprint import pprint
+import datetime
+from dateutil.relativedelta import relativedelta
+import json
+
+client = MongoClient()
+db = client.stickySulphur
+senators = db.senators
+
+def findByName(name):
+    if " " not in name:
+        return("full name required")
+    else:
+        fname = name[:name.find(" ")]
+        lname = name[name.find(" ")+1:]
+    for senator in col.find({"person.firstname": fname, "person.lastname": lname}):
+        pprint(senator)
+
+def findByGender(gender: str):
+	if gender is not "male" and gender is not "female":
+		pprint("please enter a valid gender")
+	else:
+		for senator in col.find({"person.gender": gender}):
+			pprint(senator)
+
+def findParty(party):
+    for senator in col.find({"party": party}):
+        pprint(senator)
+
+def findByAge(age):
+    now = datetime.datetime.now()
+    for senator in senators.find({}):
+        birthdayNumbers = senator["person"]["birthday"].split("-")
+        birthday = datetime.datetime(int(birthdayNumbers[0]), int(birthdayNumbers[1]), int(birthdayNumbers[2]))
+        senAge = relativedelta(now, birthday).years
+        if senAge > age:
+            pprint(senator)
+
+def findByRank(rank):
+    for senator in senators.find({"senator_rank": rank}):
+        pprint(senator)
