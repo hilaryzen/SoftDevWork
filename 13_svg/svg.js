931b8abd84b07a945b30edbc8ce671773bacb98e
Hilary Zen <hzen00@stuy.edu>
2020-03-30 21:57:26 -0400

    Change location

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -1,12 +1,11 @@
 var pic = document.getElementById("vimage");
 var button = document.getElementById("clear");
 
-// variable for coords of all circles
-var coords = [];
+// variable for tracking whether a circle has moved or changed color
 var action = false;
 
 var draw = function(e) {
-    action = false;
+  if (action == false) {
     // makes new object so old one doesn't get overwritten
     var c = document.createElementNS(
         "http://www.w3.org/2000/svg", "circle");
@@ -23,28 +22,38 @@ var draw = function(e) {
     c.setAttribute("r", "20");
     c.setAttribute("fill", "blue");
     pic.appendChild(c);
-
+  } else {
+    // if mouse already clicked on a circle, nothing is drawn
+    action = false;
+  }
 };
 
 var change = function(e) {
   x = e.offsetX;
   y = e.offsetY;
-  circles = pic.children;
+  circles = pic.children; // gets collection of all circles
   i = circles.length - 1;
+  // check if mouse has clicked on any circles, starting from the last one
   while (i >= 0 && action == false) {
     c = circles.item(i);
-    //console.log(c.getAttribute("cx"));
     if (dist(x, y, c.getAttribute("cx"), c.getAttribute("cy"))) {
-      c.setAttribute("fill", "red");
+      if (c.getAttribute("fill") == "blue") {
+        c.setAttribute("fill", "red");
+      } else {
+        // send circle to new location and change it to blue
+        c.setAttribute("cx", Math.floor(Math.random() * 500));
+        c.setAttribute("cy", Math.floor(Math.random() * 500));
+        c.setAttribute("fill", "blue");
+      }
       action = true;
     }
     i--;
   }
 }
 
+// calculates distance between mouse coords x,y and circle coords
 var dist = function(x, y, cx, cy) {
   d = Math.pow(cx - x, 2) + Math.pow(cy - y, 2);
-  console.log(d <= 400);
   return d <= 400;
 }
 
